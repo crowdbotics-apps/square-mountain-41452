cfe3dd6407dd7019f58f373e141d4b1fc5bfce91
Crowdbotics <team@crowdbotics.com>
2023-06-13 18:23:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-mountain-41452",
+  url: "https://square-mountain-41452.botics.co",
+  api: "https://square-mountain-41452.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
